ba672550a17aa7124745b0d1d63da4d91b07a0c8
Artem Chervychnyk <artem@themindstudios.com>
2024-05-21 14:41:33 +0300

    chore: update report for SonarQube to use generic issue format

--- lib/brakeman/report/report_sonar.rb
+++ lib/brakeman/report/report_sonar.rb
@@ -1,38 +1,62 @@
 class Brakeman::Report::Sonar < Brakeman::Report::Base
   def generate_report
     report_object = {
+      rules: generate_rules,
       issues: all_warnings.map { |warning| issue_json(warning) }
     }
-    return JSON.pretty_generate report_object
+    JSON.pretty_generate(report_object)
   end
-  
+
   private
-  
+
+  def generate_rules
+    all_warnings.uniq(&:warning_code)
+                .map { |warning| rule_json(warning) }
+  end
+
+  def rule_json(warning)
+    {
+      id: warning.warning_code,
+      name: warning.warning_type,
+      description: warning.message,
+      engineId: 'Brakeman',
+      cleanCodeAttribute: 'FORMATTED', # This is a placeholder, adjust as necessary
+      impacts: [
+        {
+          softwareQuality: 'SECURITY',
+          severity: severity_level_for(warning.confidence)
+        }
+      ]
+    }
+  end
+
   def issue_json(warning)
     {
-      engineId: "Brakeman",
       ruleId: warning.warning_code,
-      type: "VULNERABILITY",
-      severity: severity_level_for(warning.confidence),
+      effortMinutes: effort_minutes_for(warning.confidence),
       primaryLocation: {
         message: warning.message,
         filePath: warning.file.relative,
         textRange: {
-          "startLine": warning.line || 1,
-          "endLine": warning.line || 1,
+          startLine: warning.line || 1,
+          endLine: warning.line || 1
         }
-      },
-      effortMinutes: (4 - warning.confidence) * 15
+      }
     }
   end
 
   def severity_level_for(confidence)
-    if confidence == 0
-      "CRITICAL"
-    elsif confidence == 1
-      "MAJOR"
+    case confidence
+    when 0
+      'HIGH'
+    when 1
+      'MEDIUM'
     else
-      "MINOR"
+      'LOW'
     end
+  end
+
+  def effort_minutes_for(confidence)
+    (4 - confidence) * 15
   end
 end
